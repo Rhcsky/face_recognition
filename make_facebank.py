f1a411e6b1d2c2297642c2b8097bb1ded6bd7c74
rhcsky <dev.rhcsky@gmail.com>
2021-06-05 06:42:08 +0000

    Create make_facebank

--- make_facebank.py
+++ make_facebank.py
@@ -1,17 +1,31 @@
 import os
 from glob import glob
 
+import albumentations as A
 import cv2
+import face_recognition
 import torch
-import torchvision.transforms as transforms
+from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
+
+
+def crop_face(img):
+    location = face_recognition.face_locations(img, model='hog')
+    if len(location) == 0:
+        return []
+
+    location = location[0]
+    top, right, bottom, left = location
+    img = img[top:bottom, left:right]
+    return img
+
 
 if __name__ == '__main__':
     embedding = torch.load('saved_model/embedding.pt').to('cuda')
-    transform = transforms.Compose([
-        transforms.Resize((112, 92)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=0.441875, std=0.1859375)
+    transform = A.Compose([
+        A.Resize(92, 92),
+        A.Normalize(mean=0.441875, std=0.1859375),
+        ToTensorV2(),
     ])
     classes = glob('facebank/images/*')
     print(classes)
@@ -26,6 +40,11 @@ if __name__ == '__main__':
         embed_list = list()
         for img_dir in glob(os.path.join(c, '*')):
             img = cv2.imread(img_dir, cv2.IMREAD_GRAYSCALE)
+
+            face_img = crop_face(img)
+            if len(face_img) != 0:
+                img = face_img
+
             img = transform(image=img)['image'].unsqueeze(0).float().to('cuda')
             embed_vector = embedding(img)
             embed_list.append(embed_vector.squeeze(0))
